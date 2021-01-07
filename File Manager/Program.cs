05917f30fad952f6a271b347e9d389145d9738d0
AMTitan <amtitan@icloud.com>
2021-01-07 09:55:13 -0500

    made it so that the 3rd form is the start one

--- File Manager/Program.cs
+++ File Manager/Program.cs
@@ -16,7 +16,7 @@ namespace File_Manager
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form3());
         }
     }
 }
