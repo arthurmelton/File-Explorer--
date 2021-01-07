c67930f91ca8effb552dd299f6a375eea94a1e26
AMTitan <amtitan@icloud.com>
2021-01-07 08:30:24 -0500

    made it so that it can go into subfolders from it

--- File Manager/Form1.cs
+++ File Manager/Form1.cs
@@ -12,21 +12,25 @@ namespace File_Manager
     {
 
         private readonly List<string> _files = new List<string>();
-        
+
+        private FolderBrowserDialog _folderBrowserDialog;
+
         public Form1()
         {
             InitializeComponent();
+            //var form2 = new form_2();
+            //Application.Run(form2);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             _files.Clear();
             listView1.Items.Clear();
-            using (var folderBrowserDialog = new FolderBrowserDialog { Description = @"Select your path."})
+            using (_folderBrowserDialog = new FolderBrowserDialog { Description = @"Select your path."})
             {
-                if (folderBrowserDialog.ShowDialog() != DialogResult.OK) return;
+                if (_folderBrowserDialog.ShowDialog() != DialogResult.OK) return;
 
-                foreach (var item in GetFiles(folderBrowserDialog.SelectedPath))
+                foreach (var item in GetFiles(_folderBrowserDialog.SelectedPath))
                 {
                     imageList1.Images.Add(Icon.ExtractAssociatedIcon(item) ?? throw new InvalidOperationException());
                     var fileInfo = new FileInfo(item);
@@ -34,7 +38,7 @@ namespace File_Manager
                     listView1.Items.Add(fileInfo.Name, imageList1.Images.Count - 1);
                 }
                 
-                foreach (var item in GetDirectories(folderBrowserDialog.SelectedPath))
+                foreach (var item in GetDirectories(_folderBrowserDialog.SelectedPath))
                 {
                     //imageList1.Images.Add(Icon.ExtractAssociatedIcon(item) ?? throw new InvalidOperationException());
                     var fileInfo = new FileInfo(item);
@@ -50,7 +54,43 @@ namespace File_Manager
         {
             if (listView1.FocusedItem == null) return;
 
-            if (_files != null) Process.Start(_files[listView1.FocusedItem.Index]);
+            //if (_files != null) Process.Start(_files[listView1.FocusedItem.Index]);
+        }
+
+        private void listView1_ItemActivate(object sender, EventArgs e)
+        {
+            if (listView1.FocusedItem == null) return;
+
+            var fileAttributes = File.GetAttributes(_folderBrowserDialog.SelectedPath + "/" + listView1.FocusedItem.Name);
+
+            if ((fileAttributes & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                _folderBrowserDialog.SelectedPath = _folderBrowserDialog.SelectedPath + @"/" + listView1.FocusedItem.Name;
+
+                _files.Clear();
+                listView1.Items.Clear();
+                foreach (var item in GetFiles(_folderBrowserDialog.SelectedPath))
+                {
+                    imageList1.Images.Add(Icon.ExtractAssociatedIcon(item) ?? throw new InvalidOperationException());
+                    var fileInfo = new FileInfo(item);
+                    _files.Add(fileInfo.FullName);
+                    listView1.Items.Add(fileInfo.Name, imageList1.Images.Count - 1);
+                }
+                
+                foreach (var item in GetDirectories(_folderBrowserDialog.SelectedPath))
+                {
+                    //imageList1.Images.Add(Icon.ExtractAssociatedIcon(item) ?? throw new InvalidOperationException());
+                    var fileInfo = new FileInfo(item);
+                    _files.Add(fileInfo.FullName);
+                    listView1.Items.Add(fileInfo.Name, imageList1.Images.Count - 1);
+                }
+                
+                listView1.Sort();
+            }
+            else
+            {
+                if (_files != null) Process.Start(_files[listView1.FocusedItem.Index]);
+            }
         }
     }
 }
